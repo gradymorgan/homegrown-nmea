1469d9d62f3a4b00e7f1d858acbb00b9f4c3b886
Grady Morgan <gradymorgan@gmail.com>
2015-01-25 12:50:28 -0800

    initial commit.  Copied from HomegrownMarine/boat_computer

---
+++ src/nmea.js
@@ -0,0 +1,423 @@
+//! nmea.js
+//! parse and format NMEA 0183 sentences.
+//! version : 0.1
+//! homegrownmarine.com
+
+var winston = require('winston');
+var moment = require('moment');
+var _ = require('lodash');
+
+//TODO: move to util class
+var coordinate = module.exports.coordinate = {
+    parse: function(string, cardinalDir) {
+        var value = +string;
+        var degrees = parseInt(value/100);
+        value -= degrees*100;
+        degrees += value / 60.0;
+        return (cardinalDir == 'S' || cardinalDir == 'W'?-1:1) * degrees;
+    },
+    format: function(value, latOrLon) {
+        var direction = (value >= 0 ? 'E':'W');
+        if (latOrLon === 'lat') {
+            direction = (value >= 0 ? 'N':'S');
+        }
+
+        value = Math.abs(value);
+        var degrees = parseInt(value);
+
+        //TODO: pad?
+        var string = ''+degrees+((value-degrees)*60).toFixed(6);
+
+        return [string, direction];
+    }
+};
+
+module.exports.variation = 0;
+
+var parsers = {};
+module.exports.parsers = parsers;
+
+/*Parse RMC Message - Recommended Minimum Navigation Information
+    
+    Expected format:
+                                                               12
+                   1 2       3 4        5 6   7   8    9  10 11 |
+                   | |       | |        | |   |   |    |   | |  |
+    $--RMC,hhmmss.ss,A,llll.ll,a,yyyyy.yy,a,x.x,x.x,xxxx,x.x,a*hh
+
+    1) Time (UTC) 
+    2) Status, V = Navigation receiver warning 
+    3) Latitude 
+    4) N or S 
+    5) Longitude 
+    6) E or W 
+    7) Speed over ground, knots 
+    8) Track made good, degrees true 
+    9) Date, ddmmyy
+    10) Magnetic Variation, degrees 
+    11) E or W 
+    12) Checksum
+    
+    Example Messages:
+    $GPRMC,204658,A,4740.2949,N,12228.5660,W,005.4,300.8,100410,018.2,E*63
+    $GPRMC,225426,A,4741.9098,N,12225,2008,W,005.2,318.7,220412,018.0,E*65
+    $GPRMC,152337,V,4741.0220,N,12224.3803,W,,,210412,018.2,E*74
+    $GPRMC,030000.6,A,4740.36415,N,12225.35953,W,000.02,059.5,160612,016.6,E*4A   //GPS 18X 5Hz
+    $GPRMC,030944.8,A,4738.3356,N,12221.6469,W,0.01,305.20,031014,018.2,E*75      //GPS 19X 10Hz
+
+    -- NMEA 2.3 adds another column
+    $GPRMC,214930.000,A,4741.1764,N,12224.1899,W,0.02,0.00,010614,,,D*78
+*/
+parsers.RMC = {
+    parse: function(message,parts) {
+        var data = {
+            msg: message,
+            type: 'rmc',
+            lat: coordinate.parse(parts[3],parts[4]),
+            latStr: parts[3]+','+parts[4],
+            lon: coordinate.parse(parts[5],parts[6]),
+            lonStr: parts[5]+','+parts[6],
+            time: moment.utc(parts[9]+' '+parts[1], "DDMMYY HHmmssS"),
+            variation: +parts[10] * (parts[11] == 'E'?-1:1)
+        };
+
+        //TODO: this is a side effect. apparently, those are bad
+        module.exports.variation = _.isNumber(data.variation)? data.variation:0;
+
+        if (parts[7])
+            data.sog = +parts[7];
+        if (parts[8])
+            data.cog = +parts[8];
+
+        return data;
+    }
+};
+
+/*RMB Recommended Minimum Navigation Information
+
+    Expected format:
+                                                                14
+           1   2 3    4    5       6 7        8 9  10  11  12 13 |
+           |   | |    |    |       | |        | |   |   |   | |  |
+    $--RMB,A,x.x,a,c--c,c--c,llll.ll,a,yyyyy.yy,a,x.x,x.x,x.x,A*hh
+
+    1) Status, V = Navigation receiver warning 
+    2) Cross Track error - nautical miles
+    3) Direction to Steer, Left or Right
+    4) TO Waypoint ID
+    5) FROM Waypoint ID
+    6) Destination Waypoint Latitude 
+    7) N or S
+    8) Destination Waypoint Longitude 
+    9) E or W
+    10) Range to destination in nautical miles 
+    11) Bearing to destination in degrees True 
+    12) Destination closing velocity in knots
+    13) Arrival Status, A = Arrival Circle Entered 
+    14) Checksum
+*/
+parsers.RMB = {
+// GPRMB,A,{xte:.2f},{dts},{from_wp},{to_wp},{lat},{latDir},{lon},{lonDir},{dtw:.1f},{btw:.0f},{vmgwp:.2f},V
+
+    format: function(data) {
+        var toLat = coordinate.format(data.to.lat, 'lat');
+        var toLon = coordinate.format(data.to.lon, 'lon');
+
+        var parts = ["IIRMB",
+            "A",
+            data.xte.toFixed(2),
+            data.dts,
+            data.to.name,
+            data.from.name,
+            toLat[0],
+            toLat[1],
+            toLon[0],
+            toLon[1],
+            data.dtw.toFixed(1),
+            data.btw.toFixed(0),
+            data.vmgw.toFixed(2),
+            'V'
+        ];
+
+        return parts.join(',');
+    }
+};
+
+/*Parse HDG message - Heading, Deviation & Variation
+    
+    Expected format:
+    
+             1   2 3   4 5  6
+             |   | |   | |  | 
+    $--HDG,x.x,x.x,a,x.x,a*hh
+    
+    1) Magnetic Sensor heading in degrees
+    2) Magnetic Deviation, degrees
+    3) Magnetic Deviation direction, E = Easterly, W = Westerly
+    4) Magnetic Variation degrees
+    5) Magnetic Variation direction, E = Easterly, W = Westerly
+    6) Checksum
+
+    Example Messages:
+    $IIHDG,298,,,18,E*18
+    $HCHDG,192.5,0.0,E,,*26
+*/
+parsers.HDG = {
+    parse: function(message,parts) {
+        return {
+            msg: message,
+            type: 'hdg',
+            hdg: +parts[1] - module.exports.variation
+        };
+    }
+};
+
+
+/*
+Parse MWV Message - Wind Speed and Angle
+    
+    Expected format:
+
+             1 2   3 4  5
+             | |   | |  | 
+    $--MWV,x.x,a,x.x,a*hh
+    
+    1) Wind Angle, 0 to 360 degrees 
+    2) Reference, R = Relative, T = True 
+    3) Wind Speed 
+    4) Wind Speed Units, K/M/N 
+    5) Status, A = Data Valid 
+    6) Checksum
+    
+    Example Messages:
+    $CRMWV,35.5,R,,,A*7F
+    $IIMWV,27.71,R,14.03,N,A*38
+*/
+parsers.MWV = {
+    parse: function(message, parts) {
+        var data = {
+            msg: message,
+            type: 'mwv',
+            ref: parts[2]
+        };
+
+        if ( parts[2] == 'R' ) {
+            angle = 'awa';
+            speed = 'aws';
+        }
+        else {
+            // angle = 'twa';
+            // speed = 'tws';
+
+            //not accepting twa from instruments
+            return null;
+        }
+
+        data[angle] = +parts[1];     //assumes K, TODO: unit conversion
+        data[speed] = +parts[3];
+
+        if (data[angle] > 180) {
+            data[angle] = -1 * (360 - data[angle]);
+        }
+        
+        return data;
+    },
+    format: function() {
+        var parts = [
+            "IIMWV",
+            data.twa.toFixed(1),
+            "T",
+            data.tws.toFixed(2),
+            "N",
+            "A"
+        ];
+
+        return parts.join(',');
+    }
+};
+
+/*
+Parse VHW Message - Water Speed and Heading
+    
+    Expected Format:
+
+             1 2   3 4   5 6   7 8  9
+             | |   | |   | |   | |  | 
+    $--VHW,x.x,T,x.x,M,x.x,N,x.x,K*hh
+
+    1) Degress True 
+    2) T = True 
+    3) Degrees Magnetic 
+    4) M = Magnetic 
+    5) Knots (speed of vessel relative to the water) 
+    6) N = Knots 
+    7) Kilometers (speed of vessel relative to the water) 
+    8) K = Kilometres 
+    9) Checksum
+    
+    Example Messages:
+    $CRVHW,,,,,6.1,N,,*3F
+    $IIVHW,,,,,4.87,N,9.03,K*4D
+*/
+parsers.VHW = {
+    parse: function(message,parts) {
+        return {
+            msg: message,
+            type: 'vhw',
+            speed: +parts[5]  //TODO: may need to unit convert if [5] isn't populated
+        };
+    }
+};
+
+/*
+Parse or format VDR Message - Set and Drift
+    
+    Expected Format:
+             1 2   3 4   5 6  7
+             | |   | |   | |  | 
+    $--VDR,x.x,T,x.x,M,x.x,N*hh
+
+    1) Degress True 
+    2) T = True 
+    3) Degrees Magnetic 
+    4) M = Magnetic 
+    5) Knots (speed of current) 
+    6) N = Knots 
+    7) Checksum
+*/
+parsers.VDR = {
+    parse: function(message,parts) {
+        return {
+            msg: message,
+            type: 'vdr',
+            set: parts[1],
+            drift: parts[5]
+        };
+    },
+    format: function(data) {
+        var parts = ["IIVDR",
+            data.set.toFixed(2),
+            "T",
+            "",
+            "",
+            data.drift.toFixed(2),
+            "N"
+        ];
+
+        return parts.join(',');
+    }
+};
+
+/*Parse ROT Message - Rate Of Turn
+    
+    Expected Format:
+             1 2  3 
+             | |  | 
+    $--ROT,x.x,A*hh
+    
+    1) Rate Of Turn, degrees per minute, "-" means bow turns to port 
+    2) Status, A means data is valid 
+    3) Checksum
+    
+    Example Messages:
+    $TIROT,-20.3,A*27 */
+parsers.ROT = {
+    parse: function(message, parts) {
+        return {
+            msg: message,
+            type: 'rot',
+            rot: +parts[1]
+        };
+    }
+};
+
+
+// $IIDPT,075.1,-1.0,*43
+parsers.DPT = {
+    parse: function(message, parts) {
+        return {
+            msg: message,
+            type: 'dpt',
+            depth: +parts[1]        //TODO: more
+        };
+    }
+};
+
+// $YXXDR,A,0.4,D,PTCH,A,-4.0,D,ROLL*70
+parsers.XDR = {
+    parse: function(message, parts) {
+        return {
+            msg: message,
+            type: 'xdr',
+            pitch: +parts[2],
+            heel: +parts[6]
+        };
+    }
+};
+
+// Calculate nmea checksum.  Assumes $ is not passed in
+var checksum = module.exports.checksum = function(message) {
+    var csum = 0;
+    for ( var i=0; i < message.length; i++ ) {
+        csum = csum ^ message.charCodeAt(i);
+    }
+
+
+    csum = csum.toString(16);
+    if (csum.length == 1) {
+        csum = '0'+csum;
+    }
+    return csum.toUpperCase();
+};
+
+// Validate nmea0183 message, assumes first character is $.
+var validate = module.exports.validate = function(message) {
+    if ( message.charAt(message.length-3) == '*' ) {
+        return checksum(message.substring(1,message.length-3)) == message.substring(message.length-2,message.length);
+    }
+    return true;
+};
+
+module.exports.messageId = function(message) {
+    var end = message.indexOf(',');
+    return message.substring(1,end);
+};
+
+module.exports.parse = function(message) {
+    message = message.trim(); //proper messages end in \r\n
+
+    if ( !validate(message) ) {
+        return null;
+    }
+
+    try {
+        message = message.substring(1, message.length-3); //trim xsum and $
+        var type = message.substring(2, 5);
+
+        if ( type in parsers && parsers[type].parse ) {
+            var parts = message.split(',');
+            return parsers[type].parse(message, parts);
+        }
+    }
+    catch(e) {
+        winston.error('Error parsing NMEA message :', e);
+    }
+
+    return null;
+};
+
+//TODO : format string or object
+module.exports.format = function(data) {
+    var message;
+    if ( _.isString(data) ) {
+        message = data;
+    }
+    else if ( data.type && data.type.toUpperCase() in parsers && parsers[data.type.toUpperCase()].format ) {
+        message = parsers[data.type.toUpperCase()].format(data);
+    }
+
+    if (message) 
+        return '$' + message + '*' + checksum(message);
+    else
+        return null;
+};
---
+++ test/nmea.js
@@ -0,0 +1,250 @@
+var assert = require('chai').assert;
+var moment = require('moment');
+var _ = require('lodash');
+
+var nmea = require('../src/nmea.js');
+
+describe('messageId', function() {
+    it(' should get the first token', function() {
+        assert.equal(nmea.messageId('$GPRMC,204658,A,4740.2949,N,12228.56'), 'GPRMC');
+        assert.equal(nmea.messageId('$PSIM1242,030000.6,A,4740.36415'), 'PSIM1242');        
+    });
+});
+
+describe('checksum', function(){
+    it(' should calculate checksum correctly', function(){
+        assert.equal(nmea.checksum('GPRMC,204658,A,4740.2949,N,12228.5660,W,005.4,300.8,100410,018.2'), '0A');
+        assert.equal(nmea.checksum('GPRMC,030000.6,A,4740.36415,N,12225.35953,W,000.02,059.5,160612,016.6,E'), '4A');
+        assert.equal(nmea.checksum('IIHDG,298,,,18,E'), '18');
+    });
+});
+
+describe('coordinates', function() {
+    it(' should be parsed', function() {
+        assert.closeTo( nmea.coordinate.parse(4740.2949,'N'), 47.6715816, 0.000001 );
+        assert.closeTo( nmea.coordinate.parse(12228.5660,'W'), -122.4761, 0.000001 );
+        //TODO: E & S & 0.0?
+    });
+
+    it(' should be formatted', function() {
+        assert.deepEqual(nmea.coordinate.format(47.6715816,'lat'), ["4740.294896","N"]);
+        assert.deepEqual(nmea.coordinate.format(-122.4761,'lon'), ["12228.566000", "W"]);
+        //TODO: E & S & 0.0?
+    });
+});
+
+describe('validate', function() {
+    it('should validate true', function() {
+        assert.ok(nmea.validate('$GPRMC,204658,A,4740.2949,N,12228.5660,W,005.4,300.8,100410,018.2*0A'));
+        assert.notOk(nmea.validate('$GPRMC,204658,A,4740.2949,N,12228.5660,W,006.4,300.8,100410,018.2*0A'));
+    });
+});
+
+describe('parsers', function() {
+    it('should not parse invalid emails', function() {
+        assert.isNull( nmea.parse('$GPRMC,204658,A,4740.2949,N,12228.5660,W,006.4,300.8,100410,018.2*0A') );
+    });
+
+    nmea.parsers.TST = {
+        parse: function(message, parts) {
+            var data = {
+                msg: message,
+                type: 'tst',
+                part1: parts[1],
+                part2: parts[2],
+                part3: parts[3],
+                part4: parts[4]
+            };
+            return data;
+        },
+        format: function(data) {
+            var parts = ['TSTST',data.part1,data.part2,data.part3,data.part4];
+            return parts.join(',');
+        }
+    };
+
+    it(' should parse', function() {
+        var actual = nmea.parse('$TSTST,part1,part2,part3,part4*50');
+        var expected = {
+            msg: 'TSTST,part1,part2,part3,part4',
+            type: 'tst',
+            part1: 'part1',
+            part2: 'part2',
+            part3: 'part3',
+            part4: 'part4'
+        };
+        assert.deepEqual(actual, expected);
+    });
+
+    it(' should format', function() {
+        var expected = '$TSTST,part1,part2,part3,part4*50';
+        var actual = nmea.format({
+            type: 'tst',
+            part1: 'part1',
+            part2: 'part2',
+            part3: 'part3',
+            part4: 'part4'
+        });
+        assert.equal(actual, expected);
+    });
+
+    it(' should format strings', function() {
+        var expected = '$PAMTC,BAUD,38400*66';
+        var actual = nmea.format('PAMTC,BAUD,38400');
+
+        assert.equal(actual, expected);
+    });
+    
+    describe('RMC', function() {
+        it('should parse', function() {
+            var expected = {
+                lat: 47.672735,
+                latStr: '4740.36415,N',
+                lon: -122.422658,
+                lonStr: '12225.35953,W',
+                time: moment.utc('160612 030000', "DDMMYY HHmmssS"),
+                variation: -16.6
+            };
+            
+            var data = nmea.parse('$GPRMC,030000,A,4740.36415,N,12225.35953,W,000.02,059.5,160612,016.6,E*52');
+
+            assert.equal(data.variation, expected.variation);
+            assert.equal(data.latStr, expected.latStr);
+            assert.equal(data.lonStr, expected.lonStr);
+            assert.closeTo(data.lat, expected.lat, 0.00001);
+            assert.closeTo(data.lon, expected.lon, 0.00001);
+            assert.equal( expected.time.diff( data.time ), 0 );
+        });
+
+        it('parsing should set the modules variation', function() {
+            var data = nmea.parse('$GPRMC,030000,A,4740.36415,N,12225.35953,W,000.02,059.5,160612,016.6,E*52');
+
+            assert.equal(-16.6, nmea.variation);
+        });
+
+        it('should parse with decimal time');
+        it('should parse without variation');
+        it('should parse without cog and sog');
+    });
+
+    describe('HDG', function() {
+        it('should parse', function() {
+            nmea.variation = 0;
+
+            var data = nmea.parse('$HCHDG,192.5,0.0,E,,*26');
+            var expected = {
+                hdg: 192.5,
+                type: 'hdg'
+            };
+
+            data = _.pick(data, _.keys(expected));
+
+            assert.deepEqual(data, expected);
+        });
+
+        it('should adjust variation when parsing.', function() {
+            nmea.variation = -16.6;
+
+            var data = nmea.parse('$HCHDG,192.5,0.0,E,,*26');
+            var expected = {
+                hdg: 209.1,
+                type: 'hdg'
+            };
+
+            data = _.pick(data, _.keys(expected));
+
+            assert.deepEqual(data, expected);
+        });
+    });
+
+    describe('VHW', function() {
+        it('should parse', function() {
+            var data = nmea.parse('$IIVHW,,,,,4.87,N,9.03,K*4D');
+            var expected = {
+                speed: 4.87,
+                type: 'vhw'
+            };
+
+            data = _.pick(data, _.keys(expected));
+
+            assert.deepEqual(data, expected);
+        });
+    });
+
+    describe('VDR', function() {
+        it('should parse');
+
+        it('should format', function() {
+            // var data = nmea.parse('$IIVHW,,,,,4.87,N,9.03,K*4D');
+            // var expected = {
+            //     speed: 4.87,
+            //     type: 'vdr'
+            // };
+
+            // data = _.pick(data, _.keys(expected));
+
+            // assert.deepEqual(data, expected);
+        });
+    });
+
+    describe('XDR', function() {
+        it('should parse', function() {
+            var data = nmea.parse('$YXXDR,A,0.4,D,PTCH,A,-4.0,D,ROLL*70');
+            var expected = {
+                pitch: 0.4,
+                heel: -4.0,
+                type: 'xdr'
+            };
+
+            data = _.pick(data, _.keys(expected));
+
+            assert.deepEqual(data, expected);
+        });
+    });
+
+    describe('DPT', function() {
+        it('should parse', function() {
+            var data = nmea.parse('$IIDPT,075.1,-1.0,*43');
+            var expected = {
+                depth: 75.1,
+                type: 'dpt'
+            };
+
+            data = _.pick(data, _.keys(expected));
+
+            assert.deepEqual(data, expected);
+        });
+    });
+
+    describe('MWV', function() {
+        it('should parse relative wind speed.', function() {
+            var data = nmea.parse('$IIMWV,153,R,07.4,N,A*17');
+            data = _.pick(data, ['awa', 'aws', 'type']);
+            var expected = {
+                awa: 153,
+                aws: 7.4,
+                type: 'mwv'
+            };
+
+            assert.deepEqual(data, expected);
+        });
+        it('should parse relative wind speed greater than 180 deg.', function() {
+            var data = nmea.parse('$WIMWV,200.0,R,11.0,N,A*11');
+            data = _.pick(data, ['awa', 'aws', 'type']);
+            var expected = {
+                awa: -160,
+                aws: 11,
+                type: 'mwv'
+            };
+
+            assert.deepEqual(data, expected);
+        });
+        it('should not parse true wind speed.', function() {
+            var data = nmea.parse('$IIMWV,153,T,07.4,N,A*17');
+            var expected = null;
+
+            assert.deepEqual(data, expected);
+        });
+    });
+});
+
